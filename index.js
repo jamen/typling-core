b86926c36d9e2dd10d6cf3ca09885ecd2028907f
Jamen Marz <jamenmarz@gmail.com>
2016-12-02 23:49:11 -0700

    Add lib/walk

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ exports.create = require('./lib/create')
 exports.verify = require('./lib/verify')
 exports.infer = require('./lib/infer')
 exports.query = require('./lib/query')
+
+// var esprima = require('esprima')
+//
+// var node = esprima.parse(`
+// // Number, Number -> Number
+// function foo (x, y) { return x + y }
+//
+// foo(1, 'two')
+// `, { attachComment: true })
+//
+// console.log(exports.check(node))
