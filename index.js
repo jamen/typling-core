a0f6d475c37356a3cd7ce7d510bbf4f29b37a61e
Jamen Marz <jamenmarz@gmail.com>
2016-12-03 14:18:45 -0700

    Fix check function

--- index.js
+++ index.js
@@ -2,14 +2,3 @@ exports.check = require('./lib/check')
 exports.create = require('./lib/create')
 exports.verify = require('./lib/verify')
 exports.util = require('./lib/util')
-
-// var esprima = require('esprima')
-//
-// var node = esprima.parse(`
-// // Number, Number -> Number
-// function foo (x, y) { return x + y }
-//
-// foo(1, 'two')
-// `, { attachComment: true, loc: true })
-//
-// console.log(exports.check(node))
